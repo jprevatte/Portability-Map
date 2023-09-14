1b0bbb61e0a4d5e0e8ecc6911e50dfeb66f74cb1
jprevatte <37543780+jprevatte@users.noreply.github.com>
2023-09-14 15:37:31 -0400

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,471 @@
+var map_cfg = {
+ "mapWidth": "100%",
+ "mapHeight": 410,
+ "shadowWidth": 2,
+ "shadowOpacity": 0.2,
+ "shadowColor": "black",
+ "shadowX": 0,
+ "shadowY": 0,
+ "iPhoneLink": true,
+ "isNewWindow": false,
+ "zoomEnable": false,
+ "zoomEnableControls": false,
+ "zoomIgnoreMouseScroll": false,
+ "zoomMax": 8,
+ "zoomStep": 0.8,
+ "borderColor": "#ffffff",
+ "borderColorOver": "#ffffff",
+ "borderOpacity": 0.5,
+ "nameColor": "#ffffff",
+ "nameColorOver": "#ffffff",
+ "nameFontSize": "10px",
+ "nameFontWeight": "bold",
+ "nameStroke": false,
+ "nameStrokeColor": "#000000",
+ "nameStrokeColorOver": "#000000",
+ "nameStrokeWidth": 1.5,
+ "nameStrokeOpacity": 0.5,
+ "overDelay": 300,
+ "nameAutoSize": false,
+ "tooltipOnHighlightIn": false,
+ "freezeTooltipOnClick": false,
+ "mapId": "vcg",
+ "map_data": {
+  "st1": {
+   "name": "Alabama",
+   "shortname": "AL",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st2": {
+   "name": "Alaska",
+   "shortname": "AK",
+   "comment": "Alaska has a Telemedicine Business Registry. All businesses that plan to engage in telemedicine services with a resident in the state must have a valid Alaska business license and submit the Telemedicine Business Registry application. Applicants must register as a business, not a licensee, and several licensees can practice under one registration. Out-of-state MFT practices are included.",
+   "link": "https://www.commerce.alaska.gov/web/cbpl/ProfessionalLicensing/TelemedicineBusinessRegistry.aspx",
+   "color": "#f89a1c",
+   "colorOver": "#f89a1c"
+  },
+  "st3": {
+   "name": "Arizona",
+   "shortname": "AZ ",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st4": {
+   "name": "Arkansas",
+   "shortname": "AR",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st5": {
+   "name": "California",
+   "shortname": "CA",
+   "comment": "",
+   "link": "",
+   "color": "#3e308f",
+   "colorOver": "#3e308f"
+  },
+  "st6": {
+   "name": "Colorado",
+   "shortname": "CO",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st7": {
+   "name": "Connecticut",
+   "shortname": "CT",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st8": {
+   "name": "Delaware",
+   "shortname": "DE",
+   "comment": "Telehealth laws for healthcare providers were updated in Delaware during the fall of 2022. House Bill 334 allows MFTs and other providers licensed in a state that has not adopted an interstate compact applicable to them to provide telehealth services to Delaware residents. Providers who engage in interstate telehealth services must meet all requirements, including holding a valid license in another jurisdiction, being in good standing in all states they are licensed in, not being subject to any disciplinary proceedings, and obtaining an interstate registration from the Division of Professional Regulation.",
+   "link": "",
+   "color": "#f89a1c",
+   "colorOver": "#f89a1c"
+  },
+  "st9": {
+   "name": "District of Columbia",
+   "shortname": "DC",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st10": {
+   "name": "Florida",
+   "shortname": "FL",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st11": {
+   "name": "Georgia",
+   "shortname": "GA",
+   "comment": "",
+   "link": "",
+   "color": "#3e308f",
+   "colorOver": "#3e308f"
+  },
+  "st12": {
+   "name": "Hawaii",
+   "shortname": "HI",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st13": {
+   "name": "Idaho",
+   "shortname": "ID",
+   "comment": "Telehealth laws for behavioral health professionals were updated in Idaho during the spring of 2023. House Bill 61 allows MFTs and other licensed behavioral health professionals in another state to provide telehealth services to Idaho residents. Providers who engage in interstate telehealth services must meet all requirements, including holding a valid license in another jurisdiction that has similar licensure requirements to Idaho, not being subject to any disciplinary proceedings, acting in compliance with Idaho laws and regulations, and biennially completing the Interstate Mental or Behavioral Telehealth registration.",
+   "link": "",
+   "color": "#f89a1c",
+   "colorOver": "#f89a1c"
+  },
+  "st14": {
+   "name": "Illinois",
+   "shortname": "IL",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st15": {
+   "name": "Indiana",
+   "shortname": "IN",
+   "comment": "Telehealth laws were updated in Indiana during the spring of 2022. Senate Bill 284 expands the definition of “practitioners,” allowing more out-of-state professionals to provide telehealth services to Indiana residents.",
+   "link": "https://iga.in.gov/legislative/2022/bills/senate/284/details",
+   "color": "#f89a1c",
+   "colorOver": "#f89a1c"
+  },
+  "st16": {
+   "name": "Iowa",
+   "shortname": "IA",
+   "comment": "",
+   "link": "",
+   "color": "#3e308f",
+   "colorOver": "#3e308f"
+  },
+  "st17": {
+   "name": "Kansas",
+   "shortname": "KS",
+   "comment": "MFT portability laws were updated in Kansas during the spring of 2023. Senate Bill 131 requires out-of-state applicants to have been licensed in another state with a similar scope of practice for at least one year. Before this bill was signed into law, applicants must have been licensed for at least four years before becoming eligible to practice in Kansas.",
+   "link": "http://www.kslegislature.org/li/b2023_24/measures/sb131/",
+   "color": "#f89a1c",
+   "colorOver": "#f89a1c"
+  },
+  "st18": {
+   "name": "Kentucky",
+   "shortname": "KY",
+   "comment": "",
+   "link": "",
+   "color": "#3e308f",
+   "colorOver": "#3e308f"
+  },
+  "st19": {
+   "name": "Louisiana",
+   "shortname": "LA",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st20": {
+   "name": "Maine",
+   "shortname": "ME",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st21": {
+   "name": "Maryland",
+   "shortname": "MD",
+   "comment": "",
+   "link": "",
+   "color": "#3e308f",
+   "colorOver": "#3e308f"
+  },
+  "st22": {
+   "name": "Massachusetts",
+   "shortname": "MA",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st23": {
+   "name": "Michigan",
+   "shortname": "MI",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st24": {
+   "name": "Minnesota",
+   "shortname": "MN",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st25": {
+   "name": "Mississippi",
+   "shortname": "MS",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st26": {
+   "name": "Missouri",
+   "shortname": "MO",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st27": {
+   "name": "Montana",
+   "shortname": "MT",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st28": {
+   "name": "Nebraska",
+   "shortname": "NE",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st29": {
+   "name": "Nevada",
+   "shortname": "NV",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st30": {
+   "name": "New Hampshire",
+   "shortname": "NH",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st31": {
+   "name": "New Jersey",
+   "shortname": "NJ",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st32": {
+   "name": "New Mexico",
+   "shortname": "NM",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st33": {
+   "name": "New York",
+   "shortname": "NY",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st34": {
+   "name": "North Carolina",
+   "shortname": "NC",
+   "comment": "",
+   "link": "",
+   "color": "#3e308f",
+   "colorOver": "#3e308f"
+  },
+  "st35": {
+   "name": "North Dakota",
+   "shortname": "ND",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st36": {
+   "name": "Ohio",
+   "shortname": "OH",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st37": {
+   "name": "Oklahoma",
+   "shortname": "OK",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st38": {
+   "name": "Oregon",
+   "shortname": "OR",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st39": {
+   "name": "Pennsylvania",
+   "shortname": "PA",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st40": {
+   "name": "Rhode Island",
+   "shortname": "RI",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st41": {
+   "name": "South Carolina",
+   "shortname": "SC",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st42": {
+   "name": "South Dakota",
+   "shortname": "SD",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st43": {
+   "name": "Tennessee",
+   "shortname": "TN",
+   "comment": "",
+   "link": "",
+   "color": "#3e308f",
+   "colorOver": "#3e308f"
+  },
+  "st44": {
+   "name": "Texas",
+   "shortname": "TX",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st45": {
+   "name": "Utah",
+   "shortname": "UT",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st46": {
+   "name": "Vermont",
+   "shortname": "VT",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st47": {
+   "name": "Virginia",
+   "shortname": "VA",
+   "comment": "",
+   "link": "",
+   "color": "#3e308f",
+   "colorOver": "#3e308f"
+  },
+  "st48": {
+   "name": "Washington",
+   "shortname": "WA",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st49": {
+   "name": "West Virginia",
+   "shortname": "WV",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st50": {
+   "name": "Wisconsin",
+   "shortname": "WI",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  },
+  "st51": {
+   "name": "Wyoming",
+   "shortname": "WY",
+   "comment": "",
+   "link": "",
+   "color": "#BDBDBD",
+   "colorOver": "#9E9E9E"
+  }
+ },
+ "points": {
+  "p0": {
+   "x": 275.872,
+   "y": 331.59,
+   "color": "#3e308f",
+   "colorOver": "#3e308f",
+   "name": "",
+   "shortname": "Access MFT's Phase 1 States (2024)",
+   "pointType": "",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  },
+  "p1": {
+   "x": 276.552,
+   "y": 351.296,
+   "color": "#f89a1c",
+   "colorOver": "#f89a1c",
+   "name": "",
+   "shortname": "States with 2023 Portability Changes",
+   "pointType": "",
+   "radius": 5,
+   "textPos": "right-middle",
+   "link": "",
+   "comment": ""
+  }
+ }
+};
