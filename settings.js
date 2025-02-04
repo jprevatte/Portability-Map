9d3fb58c586feb356fdb05a0f3d3d2ddeac05d96
jprevatte <37543780+jprevatte@users.noreply.github.com>
2025-02-04 10:47:39 -0500

    Update settings.js
    Changing Nebraska and Michigan to purple.
    - TF

--- settings.js
+++ settings.js
@@ -212,8 +212,8 @@ var map_cfg = {
    "shortname": "MI",
    "comment": "",
    "link": "",
-   "color": "#BDBDBD",
-   "colorOver": "#9E9E9E"
+   "color": "#f89a1c",
+   "colorOver": "#f89a1c"
   },
   "st24": {
    "name": "Minnesota",
@@ -252,8 +252,8 @@ var map_cfg = {
    "shortname": "NE",
    "comment": "",
    "link": "",
-   "color": "#BDBDBD",
-   "colorOver": "#9E9E9E"
+   "color": "#f89a1c",
+   "colorOver": "#f89a1c"
   },
   "st29": {
    "name": "Nevada",
